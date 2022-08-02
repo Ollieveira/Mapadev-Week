df2ac8782530f65e0c9c9b234691e3e4d047078c
Willys de Oliveira <willys_oliveira@hotmail.com>
2022-08-02 15:53:19 -0300

    Finalização
    - Finalização da parte JS, carrossel das cartas pronto.

---
+++ src/js/index.js
@@ -0,0 +1,55 @@
+/*OBJETIVO 1 - Quando clicarmos na seta de avançar temos
+  que mostrar o próximo cartão da lista.
+    - Passo 1 -> Dar um jeito de pegar o elemento HTML da
+    seta avançar.
+    - Passo 2 -> Dar um jeito de identificar o clique do 
+    usuário na seta avançar.
+    - Passo 3 -> Fazer aparecer o próximo cartão da lista.
+    - Passo 4 -> Buscar o cartão que esta selecionado e 
+    esconder.
+
+  OBJETIVO 2 - Quando clicarmos na seta de voltar temos
+  que mostrar o cartão anterior da lista.
+    - Passo 1 -> Dar um jeito de pegar o elemento HTML da
+    seta voltar.
+    - Passo 2 -> Dar um jeito de identificar o clique do 
+    usuário na seta voltar.
+    - Passo 3 -> Fazer aparecer o cartão anterior da lista.
+    - Passo 4 -> Buscar o cartão que esta selecionado e 
+    esconder.    
+*/
+
+const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.getElementById('btn-voltar');
+const cartoes = document.querySelectorAll('.cartao');
+let cartaoAtual = 0;
+
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector('.selecionado');
+  cartaoSelecionado.classList.remove('selecionado');
+}
+
+function mostrarCartao(indiceCartao) {
+  cartoes[cartaoAtual].classList.add('selecionado');
+}
+
+/*----------------- OBJETIVO 1 -------------------------- */
+btnAvancar.addEventListener('click', function () {   
+  if (cartaoAtual === cartoes.length - 1) return; 
+  
+  esconderCartaoSelecionado();
+
+  cartaoAtual++; 
+  mostrarCartao(cartaoAtual); 
+});
+
+/*----------------- OBJETIVO 2 -------------------------- */
+btnVoltar.addEventListener('click', function () {
+
+  if (cartaoAtual === 0) return;
+   
+  esconderCartaoSelecionado();    
+
+  cartaoAtual--;
+  mostrarCartao(cartaoAtual); 
+});
